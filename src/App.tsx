26dfd6664ae9b48bb47c3a2f8767312531d687ce
Dário Junior <dariojunior04@gmail.com>
2021-12-03 16:58:06 -0300

    Estilizaçao inicial, assets e styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257E6;
+`;
+
 export function App() {
   return (
     <div className="App">
-      Hello World
+      <Title>Hello World</Title>
+      <GlobalStyle />
     </div>
   );
 }
