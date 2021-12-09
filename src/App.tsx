a67cca36a62fc6d70b083fa70cc0fc7187c2fd6a
Dário Junior <dariojunior04@gmail.com>
2021-12-09 17:23:07 -0300

    Criando componente de Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
+import Modal from 'react-modal';
+import { useState } from 'react';
+import { TransactionsTable } from './components/TransactionsTable';
+import { TransactionModal } from './components/TransactionModal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isTransactionModalOpen, setIsTransactionModalOpen] = useState(false);
+  function handleOpenTransactionModal() {
+      setIsTransactionModalOpen(true);
+  }
+  function handleCloseTransactionModal() {
+      setIsTransactionModalOpen(false);
+  }
+  
   return (
     <>
-      <Header />
+      <Header onOpenTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
+      <TransactionModal isOpen={isTransactionModalOpen} onRequestClose={handleCloseTransactionModal} />
       <GlobalStyle />
     </>
   );
